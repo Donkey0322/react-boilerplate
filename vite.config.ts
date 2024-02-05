f4cf5facd32b333e20b54dc52d6cee21809e0bfb
Donkey.Lee <donkey.lee@Donkey-In-Appier.local>
2024-02-05 18:21:36 +0800

    feat(app): initialize environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, `${process.cwd()}/env`, "");
+
+  return {
+    envDir: "env",
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_APP_PORT),
+    },
+  };
 })
